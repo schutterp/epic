27af37f246f19bd50dfea72e357bbd932f8ff02a
Ryan Schutte <ryan.schutte@civitaslearning.com>
2015-11-21 22:19:42 -0600

    basic elements in place for simple fetch and parse example

---
+++ README.md
@@ -0,0 +1,34 @@
+# Node Tutorial
+
+This is a basic web dev tutorial using *javascript, git, node, npm, html*. I am creating this for my little brother, Cody Schutte. Wish him luck on his journey to learn the secrets of the javascripts.
+
+The name `epic` was taken from NASA's DSCOVR:EPIC (Earth Polychromatic Imaging Camera) because we are taking data from their EPIC project.
+
+Where will this take us? Let's find out...
+
+## First Steps
+
+1.
+```
+$ pwd
+> ~/project
+```
+2. `$ git init epic`
+3. add a `.gitignore` file for node_modules directory
+2. `$ cd epic`
+3. `$ npm init --yes`
+4. `$ npm install htmlparser2 --save`
+5. `$ npm install request --save`
+4. create index.js
+6. `git add .`
+7. `git commit -m 'my first commit'`
+
+## Fun other stuff to come:
+
+-define npm run for babel... it should do the request and we could demonstrate passing options
+-use trumpet
+-build a web interface
+
+
+
+
---
+++ index.js
@@ -0,0 +1,66 @@
+const request = require('request');
+const htmlParser = require('htmlparser2');
+
+// This is the name of the id of the element we are interested in
+var ID_TO_FIND = 'data_display_cont';
+// THis is the url we want to load
+var URL = 'http://epic.gsfc.nasa.gov/';
+// I have to turn on logging when the parser sees `ID_TO_FIND`
+var isLoggingOn = false;
+// Keeps track of how many nested divs we've seen since logging was turned on.
+// A stack would be another way to do it ... push/pop instead of inc/dec
+var divCt = 0
+
+var fileNameParser = new htmlParser.Parser({
+	// look for the image name
+});
+
+// request will go fetch `URL` and call the callback function when it's ready
+request(URL, function (error, response, body) {
+	// ensure the request was successful
+	if (!error && response.statusCode === 200) {
+		var parser = new htmlParser.Parser({
+			onopentag: function(name, attributes) {
+				if (name === 'div' && attributes.id === ID_TO_FIND) {
+					console.log('Found the ' + ID_TO_FIND + ' element!');
+					isLoggingOn = true;
+				}
+				else if (isLoggingOn && name === 'div') {
+					console.log(attributes.class);
+					divCt++;
+				}
+			},
+			ontext: function(text) {
+				if (isLoggingOn) {
+					console.log('-->', text);
+				}
+			},
+			onclosetag: function(name) {
+				if (isLoggingOn && name === 'div') {
+					divCt--;
+					if (divCt < 0) {
+						isLoggingOn = false
+					}
+				}
+			}
+		}, {decodeEntities: true});
+
+		parser.write(body);
+		parser.end();
+	}
+});
+
+// Keeping this down here so I don't forget about this `trumpet` library.
+// It's a much cleaner api for parsing html if you are ready to work with streams.
+
+// var trumpet = require('trumpet');
+// var tr = trumpet();
+
+// tr.select('.b span', function (node) {
+// 	node.html(function (html) {
+// 		console.log(node.name + ': ' + html);
+// 	});
+// });
+
+// var fs = require('fs');
+// fs.createReadStream(__dirname + '/select.html').pipe(tr);
